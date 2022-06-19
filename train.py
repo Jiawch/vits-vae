13e9a95bb8d87b15d147515fcc997851da061f23
Jiawch <1163937170@qq.com>
2022-06-19 14:22:27 +0000

    replace cosine with l1

--- train.py
+++ train.py
@@ -157,7 +157,8 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
         loss_mel = F.l1_loss(mel, y_hat) * hps.train.c_mel
         c_kl = min(1., global_step / hps.train.c_kl)
         loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * c_kl
-        loss_attn = (1. - F.cosine_similarity(attn_q, attn_p, dim=-1).mean()) * hps.train.c_attn
+        #loss_attn = (1. - F.cosine_similarity(attn_q, attn_p, dim=-1).mean()) * hps.train.c_attn
+        loss_attn = F.l1_loss(attn_q, attn_p) * hps.train.c_attn
 
         loss_gen_all = loss_mel + loss_dur + loss_kl + loss_attn
     optim_g.zero_grad()
