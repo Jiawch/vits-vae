f99a2dc2d21292ac8a83d602749c596001eef951
Jiawch <1163937170@qq.com>
2022-06-20 18:38:07 +0000

    fix bug

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
         loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * c_kl
         loss_attn = F.kl_div(attn_p.log(), attn_q, reduction='batchmean') * hps.train.c_attn
 
-        loss_gen_all = loss_mel + loss_dur + loss_kl + loss_attn + loss_recall
+        loss_gen_all = loss_mel + loss_dur + loss_kl + loss_attn
     optim_g.zero_grad()
     scaler.scale(loss_gen_all).backward()
     scaler.unscale_(optim_g)
@@ -170,14 +170,14 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
     if rank==0:
       if global_step % hps.train.log_interval == 0:
         lr = optim_g.param_groups[0]['lr']
-        losses = [loss_mel, loss_dur, loss_kl, loss_attn, loss_recall]
+        losses = [loss_mel, loss_dur, loss_kl, loss_attn]
         logger.info('Train Epoch: {} [{:.0f}%]'.format(
           epoch,
           100. * batch_idx / len(train_loader)))
         logger.info([x.item() for x in losses] + [global_step, lr])
         
         scalar_dict = {"loss/g/total": loss_gen_all, "learning_rate": lr, "grad_norm_g": grad_norm_g}
-        scalar_dict.update({"loss/g/mel": loss_mel, "loss/g/dur": loss_dur, "loss/g/kl": loss_kl, "loss/g/attn": loss_attn, "loss/g/recall": loss_recall})
+        scalar_dict.update({"loss/g/mel": loss_mel, "loss/g/dur": loss_dur, "loss/g/kl": loss_kl, "loss/g/attn": loss_attn})
 
         image_dict = { 
             "all/mel_org": utils.plot_spectrogram_to_numpy(mel[0].data.cpu().numpy()),
